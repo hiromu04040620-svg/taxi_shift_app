c1f048773133cea2f707c9c191a332faef354276
hiromu04040620-svg <hiromu04040620@gmail.com>
2026-06-11 13:37:22 +0900

    chore: scaffold summary screen files

---
+++ lib/presentation/screens/summary/tabs/compliance_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ComplianceTab extends StatelessWidget {
+  const ComplianceTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
---
+++ lib/presentation/screens/summary/tabs/revenue_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RevenueTab extends StatelessWidget {
+  const RevenueTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
