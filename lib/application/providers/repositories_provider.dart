b71bc86b0d3ea13ce2723bcc313b70a4675ede89
hiromu04040620-svg <hiromu04040620@gmail.com>
2026-06-11 11:06:26 +0900

    feat(app): add repository providers

---
+++ lib/application/providers/repositories_provider.dart
@@ -0,0 +1,42 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../data/repositories/app_settings_repository.dart';
+import '../../data/repositories/revenues_repository_impl.dart';
+import '../../data/repositories/shift_overrides_repository.dart';
+import '../../data/repositories/shift_patterns_repository.dart';
+import '../../data/repositories/work_sessions_repository_impl.dart';
+import '../../domain/repositories/revenues_repository.dart';
+import '../../domain/repositories/work_sessions_repository.dart';
+import 'database_provider.dart';
+
+part 'repositories_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+ShiftPatternsRepository shiftPatternsRepository(Ref ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return ShiftPatternsRepositoryImpl(db.shiftPatternsDao);
+}
+
+@Riverpod(keepAlive: true)
+ShiftOverridesRepository shiftOverridesRepository(Ref ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return ShiftOverridesRepositoryImpl(db.shiftOverridesDao);
+}
+
+@Riverpod(keepAlive: true)
+WorkSessionsRepository workSessionsRepository(Ref ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return WorkSessionsRepositoryImpl(db.workSessionsDao);
+}
+
+@Riverpod(keepAlive: true)
+RevenuesRepository revenuesRepository(Ref ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return RevenuesRepositoryImpl(db.revenuesDao);
+}
+
+@Riverpod(keepAlive: true)
+AppSettingsRepository appSettingsRepository(Ref ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return AppSettingsRepositoryImpl(db.appSettingsDao);
+}
---
+++ lib/application/providers/repositories_provider.g.dart
@@ -0,0 +1,250 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repositories_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(shiftPatternsRepository)
+final shiftPatternsRepositoryProvider = ShiftPatternsRepositoryProvider._();
+
+final class ShiftPatternsRepositoryProvider
+    extends
+        $FunctionalProvider<
+          ShiftPatternsRepository,
+          ShiftPatternsRepository,
+          ShiftPatternsRepository
+        >
+    with $Provider<ShiftPatternsRepository> {
+  ShiftPatternsRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'shiftPatternsRepositoryProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$shiftPatternsRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<ShiftPatternsRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ShiftPatternsRepository create(Ref ref) {
+    return shiftPatternsRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ShiftPatternsRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<ShiftPatternsRepository>(value),
+    );
+  }
+}
+
+String _$shiftPatternsRepositoryHash() =>
+    r'11d17c4cdef9b9538e2c1dca6b44dae99429fb23';
+
+@ProviderFor(shiftOverridesRepository)
+final shiftOverridesRepositoryProvider = ShiftOverridesRepositoryProvider._();
+
+final class ShiftOverridesRepositoryProvider
+    extends
+        $FunctionalProvider<
+          ShiftOverridesRepository,
+          ShiftOverridesRepository,
+          ShiftOverridesRepository
+        >
+    with $Provider<ShiftOverridesRepository> {
+  ShiftOverridesRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'shiftOverridesRepositoryProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$shiftOverridesRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<ShiftOverridesRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  ShiftOverridesRepository create(Ref ref) {
+    return shiftOverridesRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(ShiftOverridesRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<ShiftOverridesRepository>(value),
+    );
+  }
+}
+
+String _$shiftOverridesRepositoryHash() =>
+    r'5454bfb3298358d2ab8ecb3dbe602bb72d2ae0d6';
+
+@ProviderFor(workSessionsRepository)
+final workSessionsRepositoryProvider = WorkSessionsRepositoryProvider._();
+
+final class WorkSessionsRepositoryProvider
+    extends
+        $FunctionalProvider<
+          WorkSessionsRepository,
+          WorkSessionsRepository,
+          WorkSessionsRepository
+        >
+    with $Provider<WorkSessionsRepository> {
+  WorkSessionsRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'workSessionsRepositoryProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$workSessionsRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<WorkSessionsRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  WorkSessionsRepository create(Ref ref) {
+    return workSessionsRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(WorkSessionsRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<WorkSessionsRepository>(value),
+    );
+  }
+}
+
+String _$workSessionsRepositoryHash() =>
+    r'f66afcedd700b53e3614efff11029ba2403228c7';
+
+@ProviderFor(revenuesRepository)
+final revenuesRepositoryProvider = RevenuesRepositoryProvider._();
+
+final class RevenuesRepositoryProvider
+    extends
+        $FunctionalProvider<
+          RevenuesRepository,
+          RevenuesRepository,
+          RevenuesRepository
+        >
+    with $Provider<RevenuesRepository> {
+  RevenuesRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'revenuesRepositoryProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$revenuesRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<RevenuesRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  RevenuesRepository create(Ref ref) {
+    return revenuesRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(RevenuesRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<RevenuesRepository>(value),
+    );
+  }
+}
+
+String _$revenuesRepositoryHash() =>
+    r'b6cc01f10996d9056f7d015cb33ce4db3df643ed';
+
+@ProviderFor(appSettingsRepository)
+final appSettingsRepositoryProvider = AppSettingsRepositoryProvider._();
+
+final class AppSettingsRepositoryProvider
+    extends
+        $FunctionalProvider<
+          AppSettingsRepository,
+          AppSettingsRepository,
+          AppSettingsRepository
+        >
+    with $Provider<AppSettingsRepository> {
+  AppSettingsRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'appSettingsRepositoryProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$appSettingsRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<AppSettingsRepository> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  AppSettingsRepository create(Ref ref) {
+    return appSettingsRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(AppSettingsRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<AppSettingsRepository>(value),
+    );
+  }
+}
+
+String _$appSettingsRepositoryHash() =>
+    r'020829a9a3a9772aacd5787b6d03321b7a73eaf2';
